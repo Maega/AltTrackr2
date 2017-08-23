0c3a869f7b7d72f514bd09bc323d39e3cdfb4ec1
Tristan Gauci <tristan@maeganetwork.com>
2017-08-23 19:52:29 +1000

    More work on charting system

--- AltTrackr2/frmCharts.vb
+++ AltTrackr2/frmCharts.vb
@@ -7,65 +7,62 @@ Public Class frmCharts
     Private Sub frmCharts_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Values = New ChartValues(Of Double)() From {3, 4, 6, 3, 2, 6}
 
-        Values = New ChartValues(Of ObservableValue)() From {
-        New ObservableValue(3),
-        New ObservableValue(4),
-        New ObservableValue(6),
-        New ObservableValue(3),
-        New ObservableValue(2),
-        New ObservableValue(6)
-        }
+        Values = New ChartValues(Of ObservableValue)() From {}
+        'New ObservableValue(3),
+        'New ObservableValue(4),
+        'New ObservableValue(6),
+        'New ObservableValue(3),
+        'New ObservableValue(2),
+        'New ObservableValue(6)
+        '}
 
         Dim lineSeries1 As New LineSeries()
         With lineSeries1
-            .Values = New ChartValues(Of Double)() From {5, 3, 5, 7, 3, 9}
-            .StrokeThickness = 4
-            .StrokeDashArray = New System.Windows.Media.DoubleCollection(New Double() {2})
-            .Stroke = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(107, 185, 69))
-            .Fill = System.Windows.Media.Brushes.Transparent
-            .LineSmoothness = 0
-            .PointGeometrySize = 0
-        End With
-
-        Dim lineSeries2 As New LineSeries()
-        With lineSeries2
             .Values = Values
-            .StrokeThickness = 2
-            .Stroke = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(28, 142, 196))
-            .Fill = System.Windows.Media.Brushes.Transparent
+            .StrokeThickness = 4
+            '.StrokeDashArray = New System.Windows.Media.DoubleCollection(New Double() {2})
+            '.Stroke = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(107, 185, 69))
+            '.Fill = System.Windows.Media.Brushes.Transparent
             .LineSmoothness = 0
-            .PointGeometrySize = 15
-            .PointForeground = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(34, 46, 49))
+            .PointGeometrySize = 18
+            .Title = "XMR - High"
         End With
 
         Dim AxisX As New Axis
         Dim SeparatorX As New Separator
-
-        With SeparatorX
-            .StrokeThickness = 1
-            .StrokeDashArray = New System.Windows.Media.DoubleCollection(New Double() {2})
-            .Stroke = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(64, 79, 86))
-        End With
-
-        AxisX.IsMerged = True
-        AxisX.Separator = SeparatorX
-
         Dim AxisY As New Axis
         Dim SeparatorY As New Separator
 
-        With SeparatorY
-            .StrokeThickness = 1.5
-            .StrokeDashArray = New System.Windows.Media.DoubleCollection(New Double() {4})
+        With SeparatorX
+            '.Step = TimeSpan.FromSeconds(1).Ticks
+            .StrokeThickness = 1
             .Stroke = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(64, 79, 86))
         End With
+
+        With SeparatorY
+            .StrokeThickness = 1
+            '.StrokeDashArray = New System.Windows.Media.DoubleCollection(New Double() {4})
+            .Stroke = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(64, 79, 86))
+        End With
+
+        AxisX.DisableAnimations = True
+        AxisX.IsMerged = True
+        AxisX.LabelFormatter = Function(value) New System.DateTime(CLng(value)).ToString("mm:ss")
+        AxisX.Separator = SeparatorX
 
         AxisY.IsMerged = True
         AxisY.Separator = SeparatorY
 
+        'cartesianChart1.AxisX.Add(New Axis() With {
+        '.DisableAnimations = True,
+        '.LabelFormatter = Function(value) New System.DateTime(CLng(value)).ToString("mm:ss"),
+        '.Separator = New Separator() With {.Step = TimeSpan.FromSeconds(1).Ticks}
+        '})
+
         With cartesianChart1
             .Series.Add(lineSeries1)
-            .Series.Add(lineSeries2)
-            .Background = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(34, 46, 49))
+            '.Series.Add(lineSeries2)
+            '.Background = New System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(34, 46, 49))
             .AxisX.Add(AxisX)
             .AxisY.Add(AxisY)
         End With
@@ -85,6 +82,6 @@ Public Class frmCharts
         Dim r As New Random
 
         Values.Add(New ObservableValue(r.Next(0, 10))) 'Add new point
-        Values.RemoveAt(0) 'Remove earliest point
+        If Values.Count > 6 Then Values.RemoveAt(0) 'Remove earliest point if there are more than six
     End Sub
 End Class
